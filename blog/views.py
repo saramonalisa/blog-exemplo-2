5e987bc7964e41e2ac357c02f4af08664acd28c6
Sara Monalisa <saramonaliscs@gmail.com>
2024-11-01 10:20:59 -0300

    Configurando formulários

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,43 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Blog, Mensagem
 
 def index(request):
     context = {
         "posts": Post.objects.all(),
+        "blog": Blog.objects.first()
     }
     return render(request, "index.html", context)
 
 def post(request, post_id):
     context = {
         "post": Post.objects.get(pk=post_id),
+        "blog": Blog.objects.first()
     }
     return render(request, "post.html", context)
 
 def sobre(request):
-    return render(request, "sobre.html")
+    context = {
+        "blog": Blog.objects.first()
+    }
+    return render(request, "sobre.html", context)
 
 def contato(request):
-    return render(request, "contato.html")
+    context = {
+            "blog": Blog.objects.first()
+            }
+    
+    if request.method == "POST":
+        print(request.POST['nome'])
+        print(request.POST['email'])
+        print(request.POST['telefone'])
+        print(request.POST['mensagem'])
+        
+        mensagem = Mensagem(nome = request.POST['nome'],
+                            email = request.POST['email'],
+                            telefone = request.POST['telefone'],
+                            mensagem = request.POST['mensagem'])
+        mensagem.save()
+        
+        return render(request, "contato.html", context)
+    else:
+        return render(request, "contato.html", context)
