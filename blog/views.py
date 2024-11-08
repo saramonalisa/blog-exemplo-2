21f784676fea1c811b28e69d98c4fa175f4ea1df
Sara Monalisa <saramonaliscs@gmail.com>
2024-11-08 10:20:32 -0300

    Adicionada validação de forms

--- blog/views.py
+++ blog/views.py
@@ -27,17 +27,36 @@ def contato(request):
             }
     
     if request.method == "POST":
-        print(request.POST['nome'])
-        print(request.POST['email'])
-        print(request.POST['telefone'])
-        print(request.POST['mensagem'])
+        context['erro'] = {} 
+        
+        if not request.POST['nome']:
+            context['erro']['nome'] = True
+        if not request.POST['email']:
+            context['erro']['email'] = True
+        if not request.POST['telefone']:
+            context['erro']['telefone'] = True
+        if not request.POST['cidade']:
+            context['erro']['cidade'] = True
+        if not request.POST['mensagem']:
+            context['erro']['mensagem'] = True
+        if context['erro']:
+            return render(request, "contato.html", context)
         
         mensagem = Mensagem(nome = request.POST['nome'],
                             email = request.POST['email'],
                             telefone = request.POST['telefone'],
+                            cidade = request.POST['cidade'],
                             mensagem = request.POST['mensagem'])
         mensagem.save()
         
         return render(request, "contato.html", context)
     else:
         return render(request, "contato.html", context)
+
+def mensagens(request):
+    context = {
+        "mensagens": Mensagem.objects.all(),
+        "blog": Blog.objects.first(),
+    }
+    
+    return render(request, "mensagens.html", context)
